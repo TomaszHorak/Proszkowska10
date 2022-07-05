9d3a5d06db26bc760d7b7d898829479e4d1367e9
TomaszHorak <52274781+TomaszHorak@users.noreply.github.com>
2022-07-05 12:37:26 +0200

    Add files via upload

--- spotify_odtwarzacz.py
+++ spotify_odtwarzacz.py
@@ -1,26 +1,24 @@
 import odtwarzacz
 import spotify_klasa
-import spotipy
-import requests
-# NAZWA_KLIENTA_SPOTIFY = 'Kodi Strych'  # taka sama podane w definicji klienta connect
-
+from MojLogger import MojLogger
 
 class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
-    def __init__(self, logger):
+    def __init__(self, obszar, logger):
         odtwarzacz.Odtwarzacz.__init__(self)
-        self.logger = logger
+        self.logger = logger    #type: MojLogger
+        self.obszar = obszar
         self.odswiez_klienta_spotify()
 
     def odswiez_klienta_spotify(self):
-        self.spot = spotify_klasa.SpotifyKlasa(self.logger)
+        self.spot = spotify_klasa.SpotifyKlasa(self.logger, 'SPOT_ODTWZRZACZ')
 
     def stop(self):
         odtwarzacz.Odtwarzacz.stop(self)
         try:
             #spot = spotify_klasa.SpotifyKlasa(self.logger)
             self.spot.klient.pause_playback(device_id=self.spot.get_dev_id())
-        except (spotipy.SpotifyException, AttributeError, requests.exceptions.ConnectionError) as serr:
-            self.logger.warning('Blad spotify stop: ' + str(serr))
+        except Exception as serr:
+            self.logger.warning(self.obszar, 'Blad spotify stop: ' + str(serr))
             self.odswiez_klienta_spotify()
         return
 
@@ -28,20 +26,19 @@ class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
         odtwarzacz.Odtwarzacz.aktualizuj_stan(self)
         try:
             pl = self.spot.klient.current_playback()
-        except (spotipy.SpotifyException, TypeError, requests.exceptions.ConnectionError,
-                requests.exceptions.ConnectTimeout, AttributeError, ValueError, Exception) as serr:
+        except Exception as serr:
             blad = str(serr)
             if 'The access token expired' in blad:
                 self.odswiez_klienta_spotify()
             else:
-                self.logger.warning('Blad spotify aktualizuj_stan: ' + blad)
+                self.logger.warning(self.obszar, 'Blad spotify aktualizuj_stan: ' + blad)
                 self.odswiez_klienta_spotify()
             try:
                 pl = self.spot.klient.current_playback()
-            except (spotipy.SpotifyException, TypeError, requests.exceptions.ConnectionError,
-                    requests.exceptions.ConnectTimeout, AttributeError, ValueError, Exception) as serr:
-                self.logger.warning('Ponowny blad spotify aktualizuj_stan: ' + str(serr))
-                self.spot.resetuj_raspotify()
+            except Exception as serr:
+                self.logger.warning(self.obszar, 'Ponowny blad spotify aktualizuj_stan: ' + str(serr))
+                #TODO ostatnio wyremowane niech  nie resetuje tak latwo
+                #self.spot.resetuj_raspotify()
                 return
         try:
             if pl is not None:
@@ -50,8 +47,8 @@ class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
                 self.totaltime = int(pl['item']['duration_ms'] / 1000)
                 self.currenttime = int(pl['progress_ms'] / 1000)
                 self.percentage = int(self.currenttime * 100 / self.totaltime)
-        except (TypeError, AttributeError, ValueError, Exception) as serr:
-            self.logger.warning('Blad spotify aktualizuj_stan type error: ' + str(serr))
+        except Exception as serr:
+            self.logger.warning(self.obszar, 'Blad spotify aktualizuj_stan type error: ' + str(serr))
         return
 
     def odtwarzaj_z_linku(self, link):
@@ -60,8 +57,8 @@ class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
             dev_id = self.spot.get_dev_id()
             powrot = self.spot.klient.start_playback(device_id=dev_id, uris=[link])
             self.spot.klient.volume(100, device_id=dev_id)
-        except (spotipy.SpotifyException, AttributeError, requests.exceptions.ConnectionError) as serr:
-            self.logger.warning('Blad spotify odtwarzaj_z_linku: ' + str(serr))
+        except Exception as serr:
+            self.logger.warning(self.obszar, 'Blad spotify odtwarzaj_z_linku: ' + str(serr))
             self.odswiez_klienta_spotify()
         return
 
@@ -72,8 +69,8 @@ class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
             dev_id = self.spot.get_dev_id()
             ms = int(czas * self.totaltime) * 10
             result = self.spot.klient.seek_track(ms, device_id=dev_id)
-        except (spotipy.SpotifyException, AttributeError, requests.exceptions.ConnectionError) as serr:
-            self.logger.warning('Blad spotify odtwarzaj_z_linku: ' + str(serr))
+        except Exception as serr:
+            self.logger.warning(self.obszar, 'Blad spotify odtwarzaj_z_linku: ' + str(serr))
             self.odswiez_klienta_spotify()
         return
 
@@ -86,7 +83,7 @@ class SpotifyOdtwarzacz(odtwarzacz.Odtwarzacz):
                 self.spot.klient.start_playback(device_id=dev_id)
             else:
                 self.spot.klient.pause_playback(device_id=dev_id)
-        except (spotipy.SpotifyException, AttributeError, requests.exceptions.ConnectionError) as serr:
-            self.logger.warning('Blad spotify play_pause: ' + str(serr))
+        except Exception as serr:
+            self.logger.warning(self.obszar, 'Blad spotify play_pause: ' + str(serr))
             self.odswiez_klienta_spotify()
         return
